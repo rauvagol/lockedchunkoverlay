2b8cd63014884a1e71d8ffc18434adc8d45f2016
Adam <akirbsy@gmail.com>
2025-10-05 14:48:39 -0700

    finalizing

--- src/test/java/com/LockedChunkOverlay/LockedChunkOverlayPluginTest.java
+++ src/test/java/com/LockedChunkOverlay/LockedChunkOverlayPluginTest.java
@@ -3,7 +3,7 @@ package com.LockedChunkOverlay;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class LockedChunkOverlayPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
